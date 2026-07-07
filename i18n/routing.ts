043915f7b301f37d869df445f4cd87cef080551f
Ahmed237-commit <aethefifthofjuly@gmail.com>
2026-07-07 12:48:44 +0300

    Edit The Languge Switcher

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,13 +1,10 @@
 import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
-export const routing = {
-    // A list of all locales that are supported
-    locales: ['en', 'ar'],
-
-    // Used when no locale matches
-    defaultLocale: 'en'
-};
+export const routing = defineRouting({
+  locales: ['en', 'ar'],
+  defaultLocale: 'en'
+});
 
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
